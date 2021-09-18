4577e6e4cf54d73cdda4f2c245d48010bc5db19b
TGab99 <55789282+TGab99@users.noreply.github.com>
2021-09-18 12:58:23 +0200

    Rewrite ThreeDBuildings.js

--- maps/src/ThreeDBuildings.js
+++ maps/src/ThreeDBuildings.js
@@ -1,41 +1,66 @@
-import React, {useRef, useState, useEffect} from 'react';
-import * as mapboxgl from 'mapbox-gl';
+import React, {useState, useRef, useEffect} from 'react';
+import * as mapboxgl from 'mapbox-gl';
 import './ThreeDBuildings.css';
 
 mapboxgl.accessToken = '';
 
 const ThreeDBuildings = () => {
     const buildingsContainerRef = useRef(null);
-    const [longitude, setLongitude] = useState(0);
-    const [latitude, setLatitude] = useState(0);
-
-    navigator.geolocation.getCurrentPosition(successPosition, errorPosition, {
-        enableHighAccuracy: true,
-    });
-
-    function successPosition(position){
-        setLongitude(position.coords.longitude);
-        setLatitude(position.coords.latitude);
-    }
-
-    function errorPosition(){
-        setLongitude(12.9716);
-        setLatitude(77.5946);
-    }
 
     useEffect(() => {
         const map = new mapboxgl.Map({
             container: buildingsContainerRef.current,
-            style: 'mapbox://styles/mapbox/light-v10',
-            center: [latitude, longitude],
+            style: "mapbox://styles/mapbox/light-v10",
+            center: [-74.0066, 40.7135],
             zoom: 15.5,
             pitch: 45,
             bearing: -17.6,
             antialias: true
+          });
+
+          map.addControl(new mapboxgl.NavigationControl(), "top-right");
+
+          map.on('load', () => {
+            const layers = map.getStyle().layers;
+            const labelLayerId = layers.find(
+                (layer) => layer.type === 'symbol' && layer.layout['text-field']
+            ).id;
+            
+            map.addLayer(
+            {
+                'id': 'add-3d-buildings',
+                'source': 'composite',
+                'source-layer': 'building',
+                'filter': ['==', 'extrude', 'true'],
+                'type': 'fill-extrusion',
+                'minzoom': 15,
+                'paint': {
+                    'fill-extrusion-color': '#aaa',
+                    'fill-extrusion-height': [
+                        'interpolate',
+                        ['linear'],
+                        ['zoom'],
+                        15,
+                        0,
+                        15.05,
+                        ['get', 'height']
+                    ],
+                    'fill-extrusion-base': [
+                        'interpolate',
+                        ['linear'],
+                        ['zoom'],
+                        15,
+                        0,
+                        15.05,
+                        ['get', 'min_height']
+                    ],
+                    'fill-extrusion-opacity': 0.6
+                }
+            },labelLayerId);
         });
 
-        return() => map.remove();
-    })
+        return () => map.remove();
+     }, []);
 
     return(
         <div>

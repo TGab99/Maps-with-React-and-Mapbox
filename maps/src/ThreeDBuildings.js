d205480b863a72b9ebabb9e959f8309754634346
TGab99 <55789282+TGab99@users.noreply.github.com>
2021-09-18 11:29:25 +0200

    Import useState and. create local variables
    I imported useState and created the local variables.

--- maps/src/ThreeDBuildings.js
+++ maps/src/ThreeDBuildings.js
@@ -1,7 +1,10 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 
 const ThreeDBuildings = () => {
     const buildingsContainerRef = useRef(null);
+    const [longitude, setLongitude] = useState(0);
+    const [latitude, setLatitude] = useState(0);
+    const [zoom, setZoom] = useState(5);
 
     return(
         <div>

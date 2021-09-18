23f877dcb4c881d97e73650fedf089905483a84e
TGab99 <55789282+TGab99@users.noreply.github.com>
2021-09-18 11:22:03 +0200

    Create ThreeDBuildings.js file

---
+++ maps/src/ThreeDBuildings.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ThreeDBuildings = () => {
+    return(
+        <div>
+            <div className="buildings_container"/>
+        </div>
+    );
+}
+
+export default ThreeDBuildings;
